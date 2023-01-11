e8df36146e0b1468b6b7c720884fabc25c62f630
jaruwan19 <stu6410014114@sskru.ac.th>
2023-01-11 11:27:21 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:healjai/splash.dart';
 
+import 'create.dart';
+import 'login.dart';
+
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     //route เป้นเส้นทางที่ใช้ในการเข้าถึงหน้าต่างๆ
       routes: {
         "/" : (context) => const Splash(),
-        // "/login":(context) => const Login(),
-        // "/create" :(context) => const Create(),
+        "/login":(context) => const Login(),
+        "/create" :(context) => const Create(),
       },
     ),
   );
